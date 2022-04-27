d264011a3908b3605cf56bd5e76e3c35cf05280f
DKonst21 <k.derienko@gmail.ru>
2022-04-27 10:10:13 +0300

    Step 1

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+def brain():
+    print("poetry run python -m brain_games.scripts.brain_games"
+          "Welcome to the Brain Games!")
+
+def main():
+    print("Welcome to the Brain Games!")
+
+if __name__ == '__main__':
+    main()
